4eb644135f2960592976a98056f0140bbd88f334
TEJESH.M <tejeshsms@gmail.com>
2025-11-18 13:27:45 +0530

    Admin-Dashboard-Version-3

--- FrontEnd/admin-Dashboard/src/main.jsx
+++ FrontEnd/admin-Dashboard/src/main.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { AuthProvider } from './context/AuthContext';
+import { ThemeModeProvider } from './context/ThemeModeContext';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <ThemeModeProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </ThemeModeProvider>
   </React.StrictMode>
 );
 
